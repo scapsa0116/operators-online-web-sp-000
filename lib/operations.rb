ee4dc71b01fdb15a7b2f0b6282f643afd11458f5
Stela Capsa <stela.capsa@gmail.com>
2019-11-11 14:35:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
+  if speed == unsafe
+    return true
+  else 
+    return false
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed = 40<=> 60
+	speed 95 ? "safe" : "unsafe"
+	return speed
 end
 	
 
