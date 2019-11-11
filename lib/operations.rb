ad6fa711fec77f64967f3d774b297dfd8112eb88
Stela Capsa <stela.capsa@gmail.com>
2019-11-11 15:48:13 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed == 40< ||speed >60
+  if speed<40 ||speed>60
     return true
   else 
     return false
@@ -10,7 +10,7 @@ end
 
 def not_safe?(speed)
 	speed ="speed == 40<||speed == > 60" == true
-	speed 40<||>60 ? "safe" : "unsafe"
+	speed<40||speed>60 ? "safe" : "unsafe"
 return speed
 end
 	
